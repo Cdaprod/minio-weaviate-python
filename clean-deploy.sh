8bf1ad74c19206b7690b0bcbd852ec1a855609b1
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-06-23 12:42:24 -0400

    Refactor script to dynamically find container names.

--- clean-deploy.sh
+++ clean-deploy.sh
@@ -30,18 +30,22 @@ docker ps
 echo "Waiting 5 seconds"
 sleep 5
 
+# Find container names dynamically
+minio_container=$(docker ps --filter "name=ts-services_minio" --format "{{.Names}}")
+weaviate_container=$(docker ps --filter "name=ts-services_weaviate" --format "{{.Names}}")
+python_app_container=$(docker ps --filter "name=ts-services_python-app" --format "{{.Names}}")
+
 echo "Docker Logs MinIO"
-docker logs --tail 20 minio_1
+docker logs --tail 20 $minio_container
 
 echo "Waiting 5 seconds"
 sleep 5
 
 echo "Docker Logs Weaviate"
-docker logs --tail 20 weaviate_1
+docker logs --tail 20 $weaviate_container
 
 echo "Waiting 5 seconds"
 sleep 5
 
 echo "Docker Logs Python-App"
-docker logs --tail 20 python-app_1
-
+docker logs --tail 20 $python_app_container
